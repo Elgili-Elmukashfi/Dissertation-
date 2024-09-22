695949646d2aabf9ada4e6158e7b91c864842310
Elgili-Elmukashfi <157979772+Elgili-Elmukashfi@users.noreply.github.com>
2024-09-22 14:58:38 +0100

    Update buffer.cpp to test it

--- buffer.cpp
+++ buffer.cpp
@@ -1,14 +1,35 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
+void buffer_overflow_function(char* input) {
     char buffer[10];
-    char input[20];
-
-    gets(input);  // Unsafe function
     strcpy(buffer, input);  // Potential buffer overflow
+}
 
+void format_string_function(char* input) {
     printf(input);  // Potential format string vulnerability
+}
+
+void array_out_of_bounds(int index) {
+    int array[5] = {1, 2, 3, 4, 5};
+    printf("%d\n", array[index]);  // Potential out of bounds access
+}
+
+int main() {
+    char input[100];
+    int index;
+
+    printf("Enter a string: ");
+    scanf("%s", input);
+    buffer_overflow_function(input);
+
+    printf("Enter a format string: ");
+    scanf("%s", input);
+    format_string_function(input);
+
+    printf("Enter an array index: ");
+    scanf("%d", &index);
+    array_out_of_bounds(index);
 
     return 0;
 }
